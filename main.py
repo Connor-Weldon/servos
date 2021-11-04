962f880f966f0294ecd45b1a25f68ab577f92260
Connor-Weldon <93712395+Connor-Weldon@users.noreply.github.com>
2021-11-04 11:40:53 +0000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,15 @@
+def on_button_pressed_a():
+    basic.show_number(0)
+    servos.P1.set_angle(0)
+    basic.pause(500)
+    basic.show_number(90)
+    servos.P1.set_angle(90)
+    basic.pause(500)
+    basic.show_number(180)
+    servos.P1.set_angle(180)
+    basic.pause(500)
+    servos.P1.stop()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+basic.show_string("servo time", 65)
+basic.show_icon(IconNames.HEART)
